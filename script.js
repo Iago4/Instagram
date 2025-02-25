68cf442f4efb9110939f105288eb601d9fa2a7bf
Iago <iago12anosbastos@gmail.com>
2025-02-25 12:56:17 -0300

    svg fix

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 function toggleMode() {
   const html = document.documentElement
-  document.getElementById("switch").addEventListener("click", function () {
-    this.name = this.name === "sunny-outline" ? "moon-outline" : "sunny-outline"
-  })
+  
 
   if (html.classList.contains("dark")) {
     html.classList.remove("dark")
     document.getElementById("logoo").src = "./.assets/Instagram_logo.svg.png"
+    document.getElementById("replace").src =
+        "./.assets/svg/moon-stars-svgrepo-com.svg"
   } else {
     html.classList.add("dark")
     document.getElementById("logoo").src =
       "./.assets/instagram-logo-white-version-png-2388x800.png"
-  }
+      document.getElementById("replace").src = "./.assets/svg/icons8-sun.svg"
+       }
 }
